58b53b64b164cd8325370e76cfb47304b4120f2f
Marcio Henrique Theodoro de Almeida <marciohtheodoro@gmail.com>
2025-09-03 14:57:32 -0300

    Aula 01 - Introdução ao backend e Node.js

---
+++ Ctrl+Young_4/servidor-basico/index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.statusCode = 200; //Opa, resposta bem-sucedida
+        res.setHeader('Content-Type', 'text/plain'); //Define o formato da resposta
+        res.end('Its, ITS, ITS ALIIIIVEE!');
+    } else if (req.url === '/amigos') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Aqui está a lista de seus amigos!');
+    } else if (req.url === '/amigo') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Esse é teu amigo?');
+    } else if (req.url === '/status') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Você está online!');
+    }
+    else {
+        res.statusCode = 404; //Código de status de para erro
+        res.setHeader('Content-Type', 'text/plain'); //Formato da resposta
+        res.end('Erro 404: Página não encontrada ;-;');
+    }
+});
+
+server.listen(2000, () => { console.log('Servidor rodando na porta 2000');});
