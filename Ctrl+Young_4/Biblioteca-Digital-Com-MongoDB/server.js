f308c2464231d0a065bed5699b571b4b4233e82d
Marcio Henrique Theodoro de Almeida <marciohtheodoro@gmail.com>
2025-09-30 17:57:23 -0300

    Adicionando Schemas do Livro e do Usuário

--- Ctrl+Young_4/Biblioteca-Digital-Com-MongoDB/server.js
+++ Ctrl+Young_4/Biblioteca-Digital-Com-MongoDB/server.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const connectDB = require("./config/config");
+const Book = require("./models/book");
 const app = express();
 connectDB();
 app.use(express.json());
 app.listen(3000, () => console.log("Server running on port 3000, yeah"));
 
-app.get("/dividir", (req, res) => {
+app.post("/api/books", async (req, res) => {
+    try{
+        const { title, author, year, genre } = req.body;
+        const newBook = new Book({ title, author, year, genre });
+        await newBook.save();
+        res.status(201).json(newBook);
+    } catch (err) {
+        res.status(500).json({ error: "Erro ao criar livro" });
+    }
+});
+
+/*app.get("/dividir", (req, res) => {
     try {
         const numerador = 10;
         const denominador = 0;
 
-        if (denominador === 0) {
+        if (denominador === 0) {    
             //res.status(400).json({ error: "Não é possível dividir por zero genius!" });
             throw new Error("Não dá pra dividir por zero!");
             //return;
@@ -21,4 +33,4 @@ app.get("/dividir", (req, res) => {
     } catch (err) {
         res.status(400).json({ error: err.message });
     }
-});
+});*/
