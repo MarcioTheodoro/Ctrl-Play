c5f3bda40cd72a925d2526f9fb69d1724a83ffe5
Marcio Theodoro <Marciohtheodoro@gmail.com>
2025-10-06 09:22:29 -0300

    Impletando o HTML e o JavaScript (servidor)

---
+++ Servidor-Alunos/server.js
@@ -0,0 +1,20 @@
+//importando o express
+const express = require('express');
+const path = require('path');
+
+//criando o app
+const app = express();
+const PORT = 3000;
+
+//dizendo pro express que a pasta "public" contém arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+//rota para o download do zip
+app.get('/download', (req, res) => {
+    const file = path.join(__dirname, 'public/arquivos/AsteroidRampage.zip');
+    res.download(file, 'AsteroidRampage.zip'); //"res.download" força o navegador a baixar
+});
+
+app.listen(PORT, () => {
+    console.log('Servidor rodando em http://localhost:${PORT}');
+});
